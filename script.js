8859fe29e426f7e5c0495fe6696aadd9bd009bbc
Jacob Larson <hockeybobber@comcast.net>
2023-10-30 17:25:55 -0500

    finished all requirements

--- script.js
+++ script.js
@@ -16,5 +16,46 @@ function onReady() {
     // - Rendered to the DOM
 }
 
+let apTotal = 100;
+
+let fungusHP = 100;
+
+function onAttackClick(event, apCost, damage) {
+   console.log('this cost you', apCost, 'this did', damage);
+
+   apTotal -= apCost;
+   fungusHP -= damage;
+
+   if (fungusHP <= 0) {
+    fungusHP = 0;
+    document.getElementById('fungusWalk').classList.replace("walk", "dead");
+   }
+
+    //if this goes over it will disable to weapon
+   if (apTotal <= apCost) {
+    apTotal = 0;
+    let allButton = document.querySelectorAll('.attack-btn');
+    for (let i = 0; i < allButton.length; i++) {
+      allButton[i].setAttribute('disabled', '')
+      document.getElementById('fungusWalk').classList.replace("walk", "jump");
+    }
+   
+} 
+
+   update()
+
+}
+
+
+function update() {
+    //brings down ap when clicking attack
+    document.getElementById('ap-text').innerHTML = apTotal + ' AP'
+    console.log('my ap total', apTotal);
+
+    //when clicked edited hp
+    document.getElementById('hp-text').innerHTML = fungusHP + ' HP'
+    console.log('the fungus hp',fungusHP);
+   
+}
 
 onReady()
